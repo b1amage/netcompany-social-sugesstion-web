63ec636bca9a6b72e4b311f233a5da7d7ee59c8c
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-04-29 14:20:43 +0700

    chore: optimize card component

--- src/components/card/PlaceCard.jsx
+++ src/components/card/PlaceCard.jsx
@@ -1,10 +1,17 @@
 import Image from "@/components/image/Image";
-import React from "react";
+import { useMemo } from "react";
 
 const PlaceCard = ({ place }) => {
   const { images, title, address, description } = place;
+  const cardSizeStyles = useMemo(
+    () =>
+      "w-[200px] h-[230px] md:w-[250px] md:h-[300px] xl:w-[400px] xl:h-[180px]",
+    []
+  );
   return (
-    <div className="w-[200px] h-[230px] md:w-[250px] md:h-[300px] xl:w-[400px] xl:h-[180px] py-3 md:py-4 xl:pt-6 px-6 bg-neutral-300 flex-center flex-col xl:flex-row xl:items-start rounded-xl gap-3 shadow-lg cursor-pointer transition-all lg:hover:-translate-y-2">
+    <div
+      className={`${cardSizeStyles} flex-col gap-3 px-6 py-3 transition-all shadow-lg cursor-pointer  md:py-4 xl:pt-6 bg-neutral-300 flex-center xl:flex-row xl:items-start rounded-xl lg:hover:-translate-y-2`}
+    >
       <Image
         src={images[0]}
         alt={title}
