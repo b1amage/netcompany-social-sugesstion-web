15e0b71d58ad69f156f6d9d765fb9f83a2dcd790
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-04-29 13:04:43 +0700

    fix: props, chore: change img to Image component

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import Portal from "@/components/HOC/Portal";
+import Image from '@/components/image/Image';
 
-const User = ({isCol, name, email, src}) => {
+const User = ({isCol, user, src}) => {
   const [isShowImage, setIsShowImage] = useState(false)
   const handleShowImage = () => {
     setIsShowImage(true)
@@ -11,16 +12,16 @@ const User = ({isCol, name, email, src}) => {
   }
   return (
     <div className={`${isCol && 'flex-col text-center'} w-fit flex items-center cursor-pointer`}>
-        <img src={src} alt='avatar' className='w-[60px] h-[60px] rounded-full' onClick={handleShowImage} />
+        <Image src={src} alt='avatar' className='w-[60px] h-[60px] !rounded-full' onClick={handleShowImage}/>
         <div className='px-4 flex flex-col'>
-            <h1 className='font-bold text-lg'>{name}</h1>
-            <p className='text-md'>{email}</p>
+            <h1 className='font-bold text-lg'>{user.name}</h1>
+            <p className='text-md'>{user.email}</p>
         </div>
 
         {isShowImage && <Portal location='html' >
           <div className="bg-black absolute inset-0" onClick={handleCloseImage}>   
           </div>
-          <img src={src} alt='avatar' className='max-w-full h-screen object-contain fixed top-0 transform translate-x-1/2 z-50' />
+          <Image src={src} alt='avatar' className='max-w-full h-screen object-contain fixed top-0 transform translate-x-1/2 z-50'/>
         </Portal>}
     </div>
   )
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,10 @@
-import User from "@/components/user/User";
-
+// import User from "@/components/user/User";
 import React from "react";
 
 const HomeScreen = () => {
-  return <div></div>;
+  return <div>
+    {/* <User src='https://scontent.fsgn12-1.fna.fbcdn.net/v/t39.30808-6/289119097_1456144428173839_7549261293075397454_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=AvucbsT3a-YAX_Mhny_&_nc_ht=scontent.fsgn12-1.fna&oh=00_AfArEeRTE1-e_tbg1JAnNtnwJt_QKRkmnw-a0lyZoT44rw&oe=6450E8B6' user={{name: 'An Bui', email:'quangan186@gmail.com'}} /> */}
+  </div>;
 };
 
 export default HomeScreen;
