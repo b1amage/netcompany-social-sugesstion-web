a214a7dd249d4d19a0cbb3d5d1885da7270757c7
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-04-29 12:55:50 +0700

    chore: remove testing code block

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,18 +1,9 @@
 import User from "@/components/user/User";
-import PlaceCard from "@/components/card/PlaceCard";
-import placeList from "@/constants/mockPlaces";
+
 import React from "react";
-import generateId from "@/utilities/generateId";
 
 const HomeScreen = () => {
-  return (
-    <div className="grid gap-5 lg:grid-cols-3 place-items-center">
-      {placeList.map((place) => (
-        <PlaceCard key={generateId()} place={place} />
-      ))}
-    </div>
-  );
-
+  return <div></div>;
 };
 
 export default HomeScreen;
