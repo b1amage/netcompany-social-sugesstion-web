2e0f7c527b3c7221c720624908b8b104097bff9a
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-04-30 22:29:12 +0700

    fix: display image size

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -39,7 +39,7 @@ const User = ({ isCol, user, src }) => {
               src={src}
               alt="avatar"
               className="cursor-auto animate-zoom h-screen flex items-center"
-              imageClassName='rounded-full w-[200px] h-[200px]'
+              imageClassName='rounded-full 2xl:w-[40vw] 2xl:h-[40vw] w-[60vw] h-[60vw]'
             />
           </div>
         </Portal>
