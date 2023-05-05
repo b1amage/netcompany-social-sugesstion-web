008627b5d9dfe046b6c028cc819502b89c5f44d9
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-05-05 14:41:15 +0700

    fix: verify

--- src/config/msalConfig.js
+++ src/config/msalConfig.js
@@ -3,7 +3,10 @@ const msalConfig = {
   auth: {
     clientId: "252884b0-5f47-4621-9f86-a5b42115a0f0",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "http://localhost:5173",
+    redirectUri:
+      process.env.NODE_ENV === "dev"
+        ? "http://localhost:5173"
+        : "https://net-social.netlify.app",
   },
   cache: {
     cacheLocation: "sessionStorage",
