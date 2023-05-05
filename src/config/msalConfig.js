66b36b1435e99f2e3e0b7c7227122c56253daf9e
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-05-05 14:52:24 +0700

    fix: redirect netlify

--- src/config/msalConfig.js
+++ src/config/msalConfig.js
@@ -2,6 +2,7 @@
 const msalConfig = {
   auth: {
     clientId: "252884b0-5f47-4621-9f86-a5b42115a0f0",
+
     authority: "https://login.microsoftonline.com/common",
     redirectUri:
       process.env.NODE_ENV === "dev"
