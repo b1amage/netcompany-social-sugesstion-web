bae639733415456d19c837b28f503034354db9b4
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-05-05 15:18:52 +0700

    fix: remove api endpoint

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -20,7 +20,7 @@ const authApi = {
       localStorage.setItem("username", tokenResponse.account.name);
 
       const backendResponse = await axiosClient.post(
-        "/api/auth/signin",
+        "/auth/signin",
         {
           microsoftIdToken: tokenResponse.idToken,
         },
