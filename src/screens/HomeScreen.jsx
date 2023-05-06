4efab9b0d0919503f5887f7e26c520914be12931
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-05-06 12:35:14 +0700

    chore: add hover effect for button

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,3 +1,4 @@
+import Button from "@/components/button/Button";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -5,7 +6,9 @@ const HomeScreen = () => {
   const { user } = useSelector((state) => state.user);
   console.log(user);
   console.log(process.env.NODE_ENV === "dev");
-  return <div className="h-screen"></div>;
+  return <div className="h-screen">
+    <Button secondary active>Hello</Button>
+  </div>;
 };
 
 export default HomeScreen;
