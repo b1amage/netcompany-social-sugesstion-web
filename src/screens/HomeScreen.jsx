8c419efd73ccf71a7ec3e18ecfc5548c954745e5
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-05-10 21:14:02 +0700

    fix:routing

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const HomeScreen = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = localStorage.getItem("user") || JSON.stringify({});
+    if (user === JSON.stringify({})) {
+      navigate("/login");
+    }
+  }, []);
   const { user } = useSelector((state) => state.user);
   console.log(user);
   console.log(process.env.NODE_ENV === "dev");
