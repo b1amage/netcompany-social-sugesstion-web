cce29bebf521672919985c53d3aded1297bbea29
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-06-01 11:43:04 +0700

    fix: component

--- src/screens/TestScreen.jsx
+++ src/screens/TestScreen.jsx
@@ -1,18 +1,20 @@
 import Screen from "@/components/container/Screen";
 import React from "react";
 import StaticMap from "@/test/StaticMap";
+import AutoCompleteScreen from "@/test/AutoComplete";
 
 const TestScreen = () => {
   return (
     <Screen>
-      <StaticMap
+      {/* <StaticMap
         title="Rmit"
         width={500}
         height={300}
         address="720 Nguyen Van Linh"
         lat={10.7289515}
         lng={106.6957667}
-      />
+      /> */}
+      <AutoCompleteScreen />
     </Screen>
   );
 };
