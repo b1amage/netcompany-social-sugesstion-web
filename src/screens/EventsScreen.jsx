4487f9d4606229d38c43d79ff14e6984ee0157f0
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-06-01 13:38:11 +0700

    fix: clean up

--- src/screens/EventsScreen.jsx
+++ src/screens/EventsScreen.jsx
@@ -2,14 +2,12 @@ import Screen from "@/components/container/Screen";
 import Heading from "@/components/typography/Heading";
 import Wrapper from "@/components/wrapper/Wrapper";
 import eventList from "@/constants/mockEvents";
-import EventCard from "@/components/card/EventCard";
-
 import React from "react";
 import useDragAndDrop from "@/hooks/useDragAndDrop";
 
 const EventsScreen = () => {
   const [renderItems] = useDragAndDrop(eventList, () => {
-    console.log("changed");
+    console.log("Call API here for updating order");
   });
   return (
     <Screen className="flex flex-col gap-5 px-3 py-4 lg:gap-10 md:px-6 md:py-5 lg:px-20">
@@ -20,10 +18,6 @@ const EventsScreen = () => {
         col="true"
         className="grid grid-cols-1 gap-2 px-2 place-items-center md:grid-cols-2 xl:grid-cols-3 md:gap-5 lg:gap-8 drag-and-drop-list"
       >
-        {/* {eventList.map((event) => (
-          <EventCard key={event.title} event={event} />
-        ))} */}
-
         {renderItems()}
       </Wrapper>
     </Screen>
