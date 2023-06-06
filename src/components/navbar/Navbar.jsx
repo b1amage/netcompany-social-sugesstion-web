e75da1c795e792c7d4e6700fb8e793b57beb28c0
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-06-06 20:04:11 +0700

    fix: logout button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -160,7 +160,7 @@ const Navbar = () => {
                   />
                 ))}
                 
-                <NavButton label="Logout" src={logout} className={`flex items-center py-4 pl-3 pr-20 mx-3 my-2 md:mx-0 md:px-4 hover:bg-gray-50/10 rounded-lg duration-200 cursor-pointer md:hover:bg-gray-200 md:bg-gray-50/10 ` } />
+                <NavButton label="Logout" src={logout} className={`!mt-auto flex items-center py-4 pl-3 pr-20 mx-3 my-2 md:mx-0 md:px-4 hover:bg-gray-50/10 rounded-lg duration-200 cursor-pointer md:hover:bg-gray-200 md:bg-gray-50/10 ` } />
                   
             </ul>
           </div>
