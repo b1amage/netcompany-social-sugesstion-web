5cf596faa8df3aef3b4172012148e418f422f4e9
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-06-11 13:36:21 +0700

    chore: fetch feature location api

--- src/api/locationApi.js
+++ src/api/locationApi.js
@@ -16,6 +16,17 @@ const locationApi = {
       return error.response.data.message;
     }
   },
+  async getFeaturedLocation(data, next_cursor){
+    try{
+      const url = `/location/filter/featured?next_cursor=${next_cursor}&locationCategory=${data.locationCategory}&searchInput=${data.searchInput}&latitude=${data.lat}&longitude=${data.lng}&searchDistance=${data.searchDistance}&weekday[openTime]=${data.weekday[0]}&weekday[closeTime]=${data.weekday[1]}&weekend[openTime]=${data.weekend[0]}&weekend[closeTime]=${data.weekend[1]}`
+      const response = await axiosClient.get(url, {
+        withCredentials: true
+      })
+      console.log(response)
+    }catch(err){
+      console.log(err)
+    }
+  }
 };
 
 export default locationApi;
