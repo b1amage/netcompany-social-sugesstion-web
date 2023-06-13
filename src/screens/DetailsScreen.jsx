8a13439aa45823f76b15cfd5cecb45ce8c76a909
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-06-13 12:33:36 +0700

    chore: remove redundant

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -71,7 +71,6 @@ const DetailsScreen = ({ event }) => {
         <LoadingScreen />
       ) : (
         <>
-          {" "}
           {/* <Image
             className="hidden xl:flex"
             src="https://images.unsplash.com/photo-1579487785973-74d2ca7abdd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80"
