042388e2ff6af0af9e125c53b6aedd19f53f7f7c
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-06-22 08:10:08 +0700

    chore: add search effect

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,25 +1,46 @@
-import Input from '@/components/form/Input'
-import React, { useState } from 'react'
-import search from '@/assets/search.svg'
-const SearchBar = ({onChange, className, wrapperClassName}) => {
-  const [value, setValue] = useState()
+import Input from "@/components/form/Input";
+import React, { useState } from "react";
+import search from "@/assets/search.svg";
+import Image from "@/components/image/Image";
+const SearchBar = ({ onChange, className, wrapperClassName }) => {
+  const [value, setValue] = useState();
+  const [isSearching, setIsSearching] = useState(false);
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      onChange(value)
+    if (event.key === "Enter" && value.trim() !== "") {
+      setIsSearching(true);
+      setTimeout(() => {
+        onChange(value);
+        setIsSearching(false);
+      }, 2000);
     }
   };
   return (
-    <div className={`w-full ${wrapperClassName}`}>
-        <Input
-            placeholder="Search"
-            className={`bg-white focus:ring-primary-400 focus:ring-1 border-primary-400 ${className}`}
-            icon={search}
-            onChange={(e) => setValue(e.target.value)}
-            onIconClick={() => onChange(value)}
-            onKeyPress={handleKeyPress}
-        />
-    </div>
-  )
-}
+    <div className={`w-full relative ${wrapperClassName}`}>
+      <Input
+        placeholder="Search"
+        className={`bg-white focus:ring-primary-400 focus:ring-1 border-primary-400 !pr-12 ${className}`}
+        // icon={search}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
 
-export default SearchBar
+      <Image
+        src={search}
+        alt="search"
+        className={`absolute w-[24px] h-[24px] top-1/2 right-4 -translate-y-3 ${
+          isSearching && "animate-bounce !top-4"
+        }`}
+        onClick={() => {
+          setIsSearching(true);
+          setTimeout(() => {
+            onChange(value);
+            setIsSearching(false);
+          }, 2000);
+        }}
+        onKeyPress={handleKeyPress}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
