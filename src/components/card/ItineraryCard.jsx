72b1338e6163fd261966049727c235748cb887a6
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-06-22 07:39:10 +0700

    feat: itinerary list

---
+++ src/components/card/ItineraryCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItineraryCard = () => {
+  return (
+    <div>ItineraryCard</div>
+  )
+}
+
+export default ItineraryCard
---
+++ src/screens/ItinerariesScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItinerariesScreen = () => {
+  return (
+    <div>ItinerariesScreen</div>
+  )
+}
+
+export default ItinerariesScreen
