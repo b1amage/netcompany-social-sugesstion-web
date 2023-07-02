b66d873a529bb834385020d6f49821b0c37def2b
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-07-02 10:01:49 +0700

    fix: get current locaiton

--- src/components/card/ItineraryCard.jsx
+++ src/components/card/ItineraryCard.jsx
@@ -29,7 +29,7 @@ const ItineraryCard = ({ name, numberOfLocations, createdAt }) => {
     <>
       <Wrapper className=" cursor-pointer hover:-translate-y-2 md:hover:-translate-y-4 duration-300 border hover:ring-1 hover:ring-primary-300 border-primary-400 bg-neutral-300 rounded-2xl justify-around items-center px-4 py-6 gap-8">
         <Wrapper col="true" className="w-full truncate">
-          <Heading className="truncate text-[16px] md:!text-[20px]">{name}...</Heading>
+          <Heading className="truncate text-[16px] md:!text-[20px]">{name}</Heading>
           <Heading className="!text-[14px] md:!text-[16px]">Locations: {numberOfLocations}</Heading>
           <SubHeading className="!text-[12px] !text-neutral-600 md:!text-[14px]">Created at: {createdAt}</SubHeading>
         </Wrapper>
