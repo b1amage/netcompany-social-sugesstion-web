0468afd333912ecc82e915fce291def407d014ed
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-07-03 22:35:28 +0700

    feat: events screen

--- src/components/card/EventCard.jsx
+++ src/components/card/EventCard.jsx
@@ -5,12 +5,23 @@ import { AiOutlineCalendar, AiOutlineClockCircle } from "react-icons/ai";
 import Text from "@/components/typography/Text";
 import Wrapper from "@/components/wrapper/Wrapper";
 import Heading from "@/components/typography/Heading";
+import { useNavigate } from "react-router-dom";
 
 const EventCard = (props) => {
+  function parseDate(isoString) {
+    const [datePart, timePart] = isoString.split("T");
+    const [yyyy, mm, dd] = datePart.split("-");
+    const [hh, mi] = timePart.split(":");
+
+    return [`${dd}/${mm}/${yyyy}`, `${hh}:${mi}`];
+  }
   const { event, className } = props;
-  const { img, title, address, description, date, time } = event;
+  const { imageUrls, name, location, startDateTime, _id } = event;
+  const [date, time] = parseDate(startDateTime);
+  const navigate = useNavigate();
   return (
     <Wrapper
+      onClick={() => navigate(`/event/${_id}`)}
       {...props}
       col="true"
       className={`px-3 py-2 rounded-lg shadow-lg md:max-w-[400px] lg:max-w-[420px] cursor-pointer ${className}`}
@@ -19,20 +30,20 @@ const EventCard = (props) => {
       <Wrapper className="gap-5 pb-3 border-b border-b-neutral-400">
         <Image
           animate
-          className="!w-[100px] !h-[100px] !min-w-[100px] lg:!w-[108px] lg:!h-[108px]"
-          src={img}
+          className="!w-[80px] !h-[80px] !min-w-[80px] lg:!w-[90px] lg:!h-[90px]"
+          src={imageUrls[0]}
         />
 
         <Wrapper col="true" className="!gap-0 max-w-1/2">
-          <Heading className="text-overflow-ellipsis">{title}</Heading>
+          <Heading className="text-overflow-ellipsis">{name}</Heading>
           <Wrapper className="items-center gap-2">
             <GoLocation />
             <SubHeading className="text-overflow-ellipsis">
-              {address}
+              {location.name}
             </SubHeading>
           </Wrapper>
 
-          <Text className="text-overflow-ellipsis-2-clamp">{description}</Text>
+          {/* <Text className="text-overflow-ellipsis-2-clamp">{description}</Text> */}
         </Wrapper>
       </Wrapper>
 
--- src/screens/AllEventsScreen.jsx
+++ src/screens/AllEventsScreen.jsx
@@ -2,16 +2,57 @@ import Screen from "@/components/container/Screen";
 import Heading from "@/components/typography/Heading";
 import Wrapper from "@/components/wrapper/Wrapper";
 import InputWithDropdown from "@/components/form/InputWithDropdown";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import eventApi from "@/api/eventApi";
 import { useNavigate } from "react-router-dom";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import Button from "@/components/button/Button";
+import Text from "@/components/typography/Text";
+
+import Select from "@/components/form/Select";
+import EventCard from "@/components/card/EventCard";
+import { debounce } from "lodash";
+import Loading from "../components/loading/Loading";
 
 const AllEventsScreen = () => {
+  const [loading, setLoading] = useState(false);
+  const [nextLoading, setNextLoading] = useState(false);
   const [suggestNextCursor, setSuggestNextCursor] = useState(undefined);
+  const [type, setType] = useState("all");
+  const [events, setEvents] = useState([]);
+  const [eventsNextCursor, setEventsNextCursor] = useState(undefined);
+  const [input, setInput] = useState("");
   const navigate = useNavigate();
+  const listRef = useRef();
+
+  useEffect(() => {
+    console.log(type);
+  }, [type]);
+
+  useEffect(() => {
+    console.log(eventsNextCursor);
+  }, [eventsNextCursor]);
+
+  useEffect(() => {
+    console.log(events);
+  }, [events]);
+
+  useEffect(() => {
+    const handleApi = async () => {
+      setLoading(true);
+      const response = await eventApi.getEvents("", type, eventsNextCursor);
+      setEventsNextCursor(response.data.next_cursor);
+      localStorage.setItem("eventsNextCursor", response.data.next_cursor);
+      setEvents(response.data.results);
+      console.log(response);
+      setLoading(false);
+    };
+    handleApi();
+  }, []);
+
   const handleGetSuggestionsSearch = (text) => {
     const handleApi = async () => {
-      const response = await eventApi.getEvents(text, "all");
+      const response = await eventApi.getEvents(text, type);
       console.log(response);
       setSuggestNextCursor(response.data.next_cursor);
       return response.data;
@@ -19,6 +60,7 @@ const AllEventsScreen = () => {
 
     return handleApi();
   };
+
   const handleSuggestionsSelect = (suggest) =>
     navigate(`/event/${suggest._id}`);
 
@@ -26,7 +68,7 @@ const AllEventsScreen = () => {
     const apiHandler = async () => {
       if (suggestNextCursor === null) return null;
       console.log(suggestNextCursor);
-      const response = await eventApi.getEvents(text, "all", suggestNextCursor);
+      const response = await eventApi.getEvents(text, type, suggestNextCursor);
       setSuggestNextCursor(response.data.next_cursor);
       return response.data;
     };
@@ -34,11 +76,34 @@ const AllEventsScreen = () => {
     return apiHandler();
   };
   return (
-    <Screen className="py-2 pb-4 xl:gap-10 xl:pb-10">
+    <Screen className="p-4 py-2 pb-5 xl:gap-10 xl:pb-10">
       <Heading>Events</Heading>
 
-      <Wrapper>
+      <Wrapper className="items-center justify-center my-5">
+        <Button
+          onClick={() => navigate(`/event/create`)}
+          active
+          className="lg:!h-[58px] flex gap-2 flex-nowrap !m-0 lg:!min-w-[250px]"
+        >
+          <AiOutlinePlusCircle className="text-2xl" />
+          <Text className="hidden lg:block">New Event</Text>
+        </Button>
         <InputWithDropdown
+          onEnter={(input) => {
+            setInput(input);
+            console.log("input", input);
+            const apiHandler = async () => {
+              const response = await eventApi.getEvents(input, type, undefined);
+
+              console.log("enter", response);
+
+              setEventsNextCursor(response.data.next_cursor);
+              setEvents(response.data.results);
+            };
+
+            apiHandler();
+          }}
+          hideLabel="true"
           handleGet={handleGetSuggestionsSearch}
           placeholder="Search event"
           onSelect={handleSuggestionsSelect}
@@ -46,8 +111,66 @@ const AllEventsScreen = () => {
           hideError="true"
           fieldToDisplay="name"
           subFieldToDisplay="startDateTime"
+          withClearButton="true"
+        />
+
+        <Select
+          onChange={(item) => {
+            setType(item);
+            const handleApi = async () => {
+              const response = await eventApi.getEvents("", item, undefined);
+              setEventsNextCursor(response.data.next_cursor);
+              setEvents(response.data.results);
+            };
+
+            handleApi();
+          }}
+          options={["all", "invited", "created", "past"]}
         />
       </Wrapper>
+
+      <Heading>Result for {input === "" ? "all" : input}</Heading>
+
+      {loading ? (
+        <Loading />
+      ) : (
+        <div
+          ref={listRef}
+          onScroll={() => {
+            const { scrollTop, scrollHeight, clientHeight } = listRef.current;
+            if (scrollTop + clientHeight === scrollHeight) {
+              console.log("Scrolled to bottom!");
+              const handleApi = debounce(async () => {
+                if (eventsNextCursor === null) return;
+                setNextLoading(true);
+                const response = await eventApi.getEvents(
+                  input,
+                  type,
+                  eventsNextCursor
+                );
+
+                console.log(response);
+                const newEvents = [...events, ...response.data.results];
+                setEventsNextCursor(response.data.next_cursor);
+                setEvents(newEvents);
+                setNextLoading(false);
+              }, 500);
+
+              handleApi();
+            }
+          }}
+          className="flex flex-col p-5 lg:h-[500px] overflow-y-scroll"
+        >
+          <div className="grid grid-cols-3 gap-4 place-items-center">
+            <>
+              {events.length > 0 &&
+                events.map((item) => <EventCard event={item} key={item._id} />)}
+
+              {nextLoading && <Loading />}
+            </>
+          </div>
+        </div>
+      )}
     </Screen>
   );
 };
