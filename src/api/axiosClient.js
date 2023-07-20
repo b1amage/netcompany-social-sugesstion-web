183cb8c2e49bf3ebea68488c2d75e38adf6425c3
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-07-20 19:49:01 +0700

    fix: proxy

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.NODE_ENV === "dev" ? "http://localhost:8080" : "/api",
-
+  baseURL:
+    process.env.NODE_ENV === "dev"
+      ? "http://localhost:8080"
+      : "https://netcompany-social-suggestion-backend.vercel.app",
   // baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
@@ -14,6 +16,11 @@ const axiosClient = axios.create({
 axios.interceptors.request.use(
   function (config) {
     // Do something before request is sent
+    const token = localStorage.getItem("token_axios");
+    if (typeof token === "string" && token != "") {
+      config.headers.Authorization = token;
+    }
+
     return config;
   },
   function (error) {
